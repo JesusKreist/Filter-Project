fd565e05592c3f199b0f81d5d462400c0d580d16
JesusKreist <59575776+JesusKreist@users.noreply.github.com>
2020-07-12 16:50:48 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,98 +1,51 @@
-// Work to get the filter buttons working
-(function(){
-    // const buttons = document.querySelectorAll('.btn')
-    // const storeItems = document.querySelectorAll('.store-item')
-    //console.log(buttons)
-    // buttons.forEach(function(button){
-    //     button.addEventListener('click', function(e){
-    //         //prevent the default link jump to top of page
-    //         e.preventDefault()
-    //         //grab the filter button that was clicked
-    //         const filter = e.target.dataset.filter
-    //         if (filter === 'all'){
-    //             //show all items
-    //             storeItems.forEach(function(item){
-    //                 item.style.display = 'block'
-    //             })
-    //         } else if (filter === 'cakes'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('cakes')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'cupcakes'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('cupcakes')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'sweets'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('sweets')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         } else if (filter === 'doughnuts'){
-    //             storeItems.forEach(function(item){
-    //                 if (item.classList.contains('doughnuts')){
-    //                     item.style.display = 'block'
-    //                 } else {
-    //                     item.style.display = 'none'
-    //                 }
-    //             })
-    //         }
-    //     })
-    // })
+const el = document.getElementsByClassName("store-item");
+const inputBar = document.getElementById("search-item");
+const filterButtons = document.getElementsByClassName("filter-btn");
 
-    // refactor to get rid of DRY code
-    const buttons = document.querySelectorAll('.btn')
-    const storeItems = document.querySelectorAll('.store-item')
+const setBlock = (event) => {
+    if (event) {
+        event.preventDefault();
+    }
+    for (let i = 0; i < 12; i++) {
+        el[i].style.display = "block";
+    }
+};
 
-    buttons.forEach((button)=> {
-        button.addEventListener('click', (e) => {
-            e.preventDefault()
-            const filter = e.target.dataset.filter
-            
-            storeItems.forEach((item)=> {
-                if (filter === 'all'){
-                    item.style.display = 'block'
-                } else {
-                    if (item.classList.contains(filter)){
-                        item.style.display = 'block'
-                    } else {
-                        item.style.display = 'none'
-                    }
-                }
-            })
-        })
-    })
 
-})();
+const filtering = (inputValue) => {
+    setBlock();
+    for (let i = 0; i < 12; i++) {
+        let storeItem = el[i].dataset.item;
+        if ((!storeItem.includes(inputValue))) {
+            el[i].style.display = "none";
+        }
+    }
+};
 
-//wire up filter search box
-(function(){
+const displayValue = (event) => {
+    if (event.keyCode === 13) {
+        setBlock();
+        let myInput = inputBar.value.toLowerCase();
+        event.preventDefault();
+        filtering(myInput);
+    }
 
-    const searchBox = document.querySelector('#search-item')
-    const storeItems = document.querySelectorAll('.store-item')
+};
 
-    searchBox.addEventListener('keyup', (e) => {
-    
-        const searchFilter = e.target.value.toLowerCase().trim()
-        //display only items that contain filter input
+const makeHandler = (param) => {
+    return function (event) {
+        event.preventDefault();
+        filtering(param);
+    };
+};
 
-        storeItems.forEach((item) => {
-            if (item.textContent.includes(searchFilter)){
-                item.style.display = 'block'
-            } else {
-                item.style.display = 'none'
-            }
-        })
-    })
 
-})();
+
+for (let i = 1; i < filterButtons.length; i++) {
+    // gets all the items i.e cakes, doughnuts... and sets them to handlerParam per loop
+    let handlerParam = filterButtons[i].dataset.filter;
+    filterButtons[i].addEventListener("click", makeHandler(handlerParam));
+};
+
+filterButtons[0].addEventListener("click", setBlock);
+inputBar.addEventListener("keyup", displayValue);
